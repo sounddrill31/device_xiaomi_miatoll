b9153fae46c5287f408d2a06cd56f7b5e5a46de7
Itu <131593996+WAR-DEPT@users.noreply.github.com>
2024-01-29 10:29:06 +0530

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm6250-common
+include device/xiaomi/sm6250-common/BoardConfigCommon.mk
+
+# Inherit proprietary blobs
+-include vendor/xiaomi/miatoll/BoardConfigVendor.mk
+
+DEVICE_PATH := device/xiaomi/miatoll
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := miatoll
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_miatoll
+TARGET_RECOVERY_DEVICE_MODULES := libinit_miatoll
+
+# HIDL
+ODM_MANIFEST_SKUS += \
+    joyeuse
+
+ODM_MANIFEST_JOYEUSE_FILES := $(DEVICE_PATH)/manifest_joyeuse.xml
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse
