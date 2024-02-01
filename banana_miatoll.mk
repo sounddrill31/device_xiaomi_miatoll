118cf3db6e4bc89551162a25b3bc36d814142def
WAR-DEPT <131593996+WAR-DEPT@users.noreply.github.com>
2024-02-01 17:55:58 +0530

    Update banana_miatoll.mk

--- banana_miatoll.mk
+++ banana_miatoll.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from the crdroid configuration.
-$(call inherit-product, vendor/banana/config/common_full_phone.mk)
+$(call inherit-product, vendor/banana/config/common.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
