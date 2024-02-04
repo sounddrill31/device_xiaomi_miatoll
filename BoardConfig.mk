13d1be7a49993dae3ae71608d937d0c2313c46e4
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-02-04 17:39:13 +0530

    miatoll: Fix path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
 
 TARGET_KERNEL_ADDITIONAL_FLAGS += LD=ld.lld AR=llvm-ar NM=llvm-nm STRIP=llvm-strip OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump LLVM_IAS=1
-TARGET_KERNEL_SOURCE := kernel/xiaomi/miatoll
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)
 TARGET_KERNEL_CONFIG := vendor/xiaomi/miatoll_defconfig
 
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0
